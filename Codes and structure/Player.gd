fcfbdaab1a00737954626f00ebb575294936b585
Kleirton Filho <kleirtonfilho@gmail.com>
2024-04-09 00:42:18 -0300

    Correção do bug da camera e adição do inimigo
    Correção do bug da camera do jogador feita pelo Yuri e adição da AI (basic) do inimigo, podendo ser incorporada a outros inimigos.

--- Codes and structure/Player.gd
+++ Codes and structure/Player.gd
@@ -1,10 +1,10 @@
 extends KinematicBody
 
-
+#Váriaveis de configurações de escopo global
 var velocity = Vector3()
 var gravity = -30
 var max_speed = 8
-var mouse_sen = 0.002
+var mouse_sensitivility = 0.002
 
 #variáveis das armas
 
@@ -14,7 +14,7 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func get_input():
-	
+	#Entradas de movimentação
 	var input_dir = Vector3()
 	if Input.is_action_pressed("move_forward"):
 		input_dir += global_transform.basis.z
@@ -24,19 +24,31 @@ func get_input():
 		input_dir += global_transform.basis.x
 	if Input.is_action_pressed("strafe_right"):
 		input_dir += -global_transform.basis.x
-	input_dir = input_dir.normalized() #cancelar stafe running
+	input_dir = input_dir.normalized() #cancelar corrida
 	return input_dir
 
+#Movimentação da camera
 func _unhandled_input(event):
-	pass
+	#Verificando se o mouse está se mexendo
+	if event is InputEventMouseMotion:
+		#Caso queira o mouse invertido, basta não tira o - em "-event"
+		rotate_y(-event.relative.x * mouse_sensitivility)
+		#Rotação do "pivô" do Player, nó que é "pai" da câmera e da arma
+		$Pivot.rotate_x(event.relative.y * mouse_sensitivility)
+		#Delimitando quantos radianos para cima e para baixo o jogador pode ver
+		#Basta altera '-1.2, 1.2', caso queira que o jogador olhe a pi/2 radianos.
+		$Pivot.rotation.x - clamp($Pivot.rotation.x,-1.2,1.2)
 	
 func _physics_process(delta):
-	#gravidade
+	#gravidade \ colisão
 	velocity.y += gravity * delta
 	var desired_velocity = get_input() * max_speed
 	velocity.x = desired_velocity.x
 	velocity.z = desired_velocity.z
 	velocity = move_and_slide(velocity, Vector3.UP, true)
+
+
+	
 func change_gun(gun):
 	pass
 	
---
+++ Codes and structure/scenes/EnemyBasicGrunt.gd
@@ -0,0 +1,47 @@
+extends KinematicBody
+
+#Obtendo todas as cenas que compõem o jogo.
+#As váriaveis do tipo onready fazem com que o programa só seja executado caso primeiro as váriaveis onready sejam executdas
+onready var nav = get_tree().get_nodes_in_group('NavMesh')[0]
+onready var player = get_tree().get_nodes_in_group('Player')[0]
+
+var path = [] #Assegura as coordenadas do caminho (NavMash) do inimigo até o jogador
+var path_index = 0 #Acompanha qual coordenada precisa ir
+var speed = 3 #Velocidade do inimigo
+var health = 20 #Quantidade de vida do inimigo
+
+
+func _ready():
+	pass
+
+func take_demage(dmg_amount): #Função que faz o inimigo levar dano
+	health -= dmg_amount
+	if health <= 0: #Caso a vida do inimigo seja 0, ele morre
+		death()
+
+func _physics_process(delta):
+	if path_index < path.size():
+		var direction = (path[path_index] - global_transform.origin)
+		if direction.length() < 1: #Vetor tridimensional que representar uma direção, distância e módulo 
+			path_index += 1
+		else:
+			move_and_slide(direction.normalized() * speed, Vector3.UP)
+	else:
+		find_path(player.global_transform.origin)
+
+func find_path(target): #Função para contrar o caminho: "guair o inimigo até o jogador".
+	path = nav.get_simple_path(global_transform.origin, target) #Essa função serve básicamente serve para encontrar o caminho entre dois pontos distintos
+	path_index = 0
+	
+func death():
+	set_process(false)
+	set_physics_process(false)
+	$CollisionShape.disabled = true
+	#Variaváveis de animações depedendo do "grau" de dano que o inimigo levar
+	if health < -20:
+		$AnimatedSprite3D.play("explode")
+	else:
+		$AnimatedSprite3D.play("die")
+
+func shoot(target):
+	pass
